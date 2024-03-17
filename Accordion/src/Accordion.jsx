5a766b31aa52043c898528c9b8582e84e2d4a8e2
Sasa Nedic <svetasmak0@gmail.com>
2024-03-17 16:41:02 +0100

    Add state

--- Accordion/src/Accordion.jsx
+++ Accordion/src/Accordion.jsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 
 const Accordion = ({question}) => {
+    const [active, setActive] = useState(false)
     return(
        
       <section>
         <div className=" "> {question.title}
             <button>+</button>
+            
         </div>
       </section>
     )
